524c61acbd6904639b684780059d3803b24816ef
Oscar Pacheco <oscarpa_2000@hotmail.com>
2022-03-30 04:03:15 -0500

    update before 2.7py

--- monitor1.py
+++ monitor1.py
@@ -1,5 +1,9 @@
+from time import sleep
+
 import zmq
 import sys
+
+from zmq.utils.strtypes import unicode
 
 
 class monitor:
@@ -7,18 +11,20 @@ class monitor:
         self.socket = None
         self.type = type
 
-    def socket_config(self):
+    def socket_config(self, type):
         context = zmq.Context()
         self.socket = context.socket(zmq.SUB)
         self.socket.connect('tcp://127.0.0.1:2001')
-        self.socket.setsockopt_string(zmq.SUBSCRIBE, self.type)
+        self.socket.setsockopt_string(zmq.SUBSCRIBE, "")
 
-    def open(self):
+    def open(self,type):
+        self.socket_config(type)
         while True:
-            message = self.socket.recv_pyobj()
-            print(message)
-
+            sleep(1)
+            publisher_id, self.type, value = self.socket.recv_pyobj()
+            print(f"server_{publisher_id} {self.type} {value}")
 
 
 if __name__ == "__main__":
     monitor1 = monitor(sys.argv[1])
+    monitor1.open(type)
